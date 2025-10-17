14f5f1802bb8699cba6a0c9c222e8b2e73cb40ca
Andreas Hoefer <andreas_hoefer@cdemy.de>
2025-10-17 23:43:21 +0200

    Update app name and related configurations to i12_intro_012

---
+++ android/app/src/main/kotlin/com/example/i12_into_012/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.i12_into_012
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
--- windows/runner/main.cpp
+++ windows/runner/main.cpp
@@ -27,7 +27,7 @@ int APIENTRY wWinMain(_In_ HINSTANCE instance, _In_opt_ HINSTANCE prev,
   FlutterWindow window(project);
   Win32Window::Point origin(10, 10);
   Win32Window::Size size(1280, 720);
-  if (!window.Create(L"i12_solution_011", origin, size)) {
+  if (!window.Create(L"i12_intro_012", origin, size)) {
     return EXIT_FAILURE;
   }
   window.SetQuitOnClose(true);
